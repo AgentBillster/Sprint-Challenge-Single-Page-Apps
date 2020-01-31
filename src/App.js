efa75449a24c0f6b2db571473f4ec47f0cca54c3
unknown <william.z.wilson95@gmail.com>
2020-01-31 13:28:49 -0600

    cant do the searchbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList"
+import { Route } from "react-router-dom"
+import Navigation from "./components/Navigation.js";
+import SearchForm from "./components/SearchForm.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Navigation />
+      <SearchForm />
+      <Route exact path="/"> <WelcomePage /> </Route>
+      <Route path="/CharacterList"> <CharacterList /> </Route>
     </main>
   );
 }
